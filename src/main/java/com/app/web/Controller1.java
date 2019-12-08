a658f6aa35bd6bc926357eb4ecac7c5f9358d233
DominatorProgramator <st49522@student.upce.cz>
2019-12-08 18:19:14 +0100

    Login form

--- src/main/java/com/app/web/Controller1.java
+++ src/main/java/com/app/web/Controller1.java
@@ -2,13 +2,19 @@ package com.app.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Controller1 {
 
     @RequestMapping("/")
     public String actionHome() {
-        return "hello";
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
